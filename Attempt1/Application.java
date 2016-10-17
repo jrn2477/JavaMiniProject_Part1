0727445a5d5d4f45437dac3452415be298dc5427
jrn2477 <jrn2477@rit.edu>
2016-10-17 09:57:51 -0400

    Some other stuff

---
+++ Attempt1/Application.java
@@ -0,0 +1,13 @@
+public class Application {
+	
+	// main method 
+	public static void main(String[] args) {
+		
+	}
+	
+	// default constructor 
+	public Application() {
+		
+	}
+	
+}
---
+++ Attempt1/Player.java
@@ -0,0 +1,7 @@
+public class Player {
+	
+	// default constructor 
+	public class Player() {
+		
+	}
+}
